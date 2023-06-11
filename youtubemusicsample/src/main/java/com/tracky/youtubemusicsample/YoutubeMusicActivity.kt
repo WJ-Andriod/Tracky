cbbbf33278778da8900633df06b3725b780c9350
WooJoong Nam <treemiddle@musinsa.com>
2023-06-11 17:36:06 +0900

    youtube music compose sample project add

---
+++ youtubemusicsample/src/main/java/com/tracky/youtubemusicsample/YoutubeMusicActivity.kt
@@ -0,0 +1,46 @@
+package com.tracky.youtubemusicsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tracky.youtubemusicsample.ui.theme.TrackyTheme
+
+class YoutubeMusicActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TrackyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Youtube")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TrackyTheme {
+        Greeting("Android")
+    }
+}
